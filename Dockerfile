b904e2e2277ba01d7a3b1285fbb947e292e1e66f
Ra_Rauw <110437253+RoshanKumar770@users.noreply.github.com>
2025-06-21 16:06:22 +0530

    updated files

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,11 @@ FROM python:3.12-slim
 WORKDIR /app
 
 COPY requirements.txt .
+
 RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+COPY app.py .
+
+EXPOSE 3000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "👋 Hello from ECS Fargate!"
+    return 'Hello, Flask on Docker!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}! Welcome to Flask on Docker.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
